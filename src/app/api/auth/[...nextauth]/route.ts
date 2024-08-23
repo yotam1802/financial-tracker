47904a0c53d426b11e51a8a040d482331ab25496
yotam1802 <Yotam1802@gmail.com>
2024-08-23 00:01:24 -0600

    Add default categories

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -20,6 +20,67 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
+  events: {
+    createUser: async ({ user }) => {
+      const defaultCategories = [
+        {
+          name: "General",
+          icon: "📦",
+          bgColor: "bg-primary-content",
+          badgeColor: "bg-primary-content",
+        },
+        {
+          name: "Food",
+          icon: "🍔",
+          bgColor: "bg-green-500",
+          badgeColor: "bg-green-300",
+        },
+        {
+          name: "Transport",
+          icon: "🚊",
+          bgColor: "bg-sky-500",
+          badgeColor: "bg-sky-300",
+        },
+        {
+          name: "Entertainment",
+          icon: "🎭",
+          bgColor: "bg-orange-500",
+          badgeColor: "bg-orange-300",
+        },
+        {
+          name: "Travel",
+          icon: "🚀",
+          bgColor: "bg-amber-500",
+          badgeColor: "bg-amber-300",
+        },
+        {
+          name: "Bills",
+          icon: "💵",
+          bgColor: "bg-lime-500",
+          badgeColor: "bg-lime-300",
+        },
+        {
+          name: "Work",
+          icon: "💼",
+          bgColor: "bg-stone-500",
+          badgeColor: "bg-stone-300",
+        },
+        {
+          name: "Investments",
+          icon: "📈",
+          bgColor: "bg-gray-500",
+          badgeColor: "bg-gray-300",
+        },
+      ];
+
+      await prisma.category.createMany({
+        data: defaultCategories.map((category) => ({
+          ...category,
+          userId: user.id,
+        })),
+      });
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
